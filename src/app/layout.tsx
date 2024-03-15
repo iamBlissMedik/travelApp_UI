99cfd338628bd0ebd8cb3ffdc38ece48ce386c7e
Blizz <ellamsbliss@gmail.com>
2024-03-15 03:50:18 +0100

    travel app ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Travel",
+  description: "Travel UI/UX App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="overflow-hidden relative">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
